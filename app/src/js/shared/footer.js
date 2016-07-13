46f329b18af67745aba05eb2fb23caa3413eb420
Kenneth Song <syltaxue@hotmail.com>
2016-07-13 10:34:17 +0800

    Update footer

--- app/src/js/shared/footer.js
+++ app/src/js/shared/footer.js
@@ -6,8 +6,8 @@ var Footer = React.createClass({
 		return (
 			<div className = "footer">
 				<div className = "footer-container">
-					<div>©2015 canadaBeautyPlus.com 电信与信息业务经营许可证000000号 沪ICP备00000000号-1</div>
-					<div>首页 | 商务合作 | 联系我们 | 地址：| 电话：</div>	
+					<div>©Designed and Built by Kenneth Song</div>
+					<div>Last Updated: 2016, July 12th</div>	
 				</div>
 			</div>
 		);
