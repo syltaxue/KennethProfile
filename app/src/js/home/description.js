7c857a6b3f48715277b1425fb4a7c679cd434e08
Kenneth Song <syltaxue@hotmail.com>
2016-07-13 13:40:38 +0800

    Use downloaded bootstrap, update description

--- app/src/js/home/description.js
+++ app/src/js/home/description.js
@@ -3,6 +3,7 @@ var Description = React.createClass({
 	render: function() {
 		return (
 			<div className = "description">
+				<img id="description-icon" src="./../app/src/image/react2.png" height="100px" width="100px"></img>
 			</div>
 		);
 	},
