5e0edc97e15975f966b3f9530fb515ad3903d370
Kenneth Song <syltaxue@hotmail.com>
2016-07-14 11:11:30 +0800

    Update contexts in the pictures and adjust some colors of the background image

--- app/src/js/shared/Picture.js
+++ app/src/js/shared/Picture.js
@@ -13,23 +13,29 @@ var Picture = React.createClass({
 						<img width={width} height={height} className="navigator-picture" alt="900x400" src="./../app/src/image/background.jpg"/>
 						<img id="description-icon" src="./../app/src/image/ubc2.gif" height="100px" width="100px"></img>
 						<Carousel.Caption>
-							<h3>Studying at UBC as a software enginnering student</h3>
+							<h3 className="text-header">Studying at UBC as a software enginnering student</h3>
 							<p>Graduating in May 2017</p>
+							<p>Had a 20-month Co-op experience</p>
 							<p>Will be available for a full-time position</p>
 						</Carousel.Caption>
 					</Carousel.Item>
 					<Carousel.Item>
-						<img width={width} height={height} className="navigator-picture" alt="900x400" src="./../app/src/image/ai.jpg"/>
+						<img width={width} height={height} className="navigator-picture" alt="900x400" src="./../app/src/image/ai2.jpg"/>
 						<Carousel.Caption>
-							<h3>Work Perference: AI</h3>
-							<p>图片文字简介2，文字介绍2</p>
+							<h3 className="text-header">Work Perference: AI</h3>
+							<p>I beleive, in the future, tons of smart devices will make our life easier</p>
+							<p>I beleive AI applications will become more and more common in our life</p>
+							<p>I wish I can have a chance to work on projects in this field</p>
+							<p>My past experience can prove that I have the potentials to learn the new skills required here</p>
 						</Carousel.Caption>
 					</Carousel.Item>
 					<Carousel.Item>
-						<img width={width} height={height} className="navigator-picture" alt="900x400" src="./../app/src/image/machineLearning3.jpg"/>
+						<img width={width} height={height} className="navigator-picture" alt="900x400" src="./../app/src/image/machineLearning1.jpg"/>
 						<Carousel.Caption>
-							<h3>Work Preference: Machine Learning</h3>
-							<p>I would like to get involved in</p>
+							<h3 className="text-header">Work Preference: Machine Learning</h3>
+							<p>I believe, in the future, most of the messages people receive will be personalized</p>
+							<p>I believe Machine learning will be applied in most applications</p>
+							<p>Although I do not have much work experience in this field, I hope I can get a chance to learn</p>
 						</Carousel.Caption>
 					</Carousel.Item>
 				</Carousel>
