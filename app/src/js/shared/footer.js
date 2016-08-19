3ffcb2a45520e57acf4a079a93640abaa103373f
Kenneth Song <syltaxue@hotmail.com>
2016-08-19 15:39:56 +0800

    last updated date

--- app/src/js/shared/footer.js
+++ app/src/js/shared/footer.js
@@ -7,7 +7,7 @@ var Footer = React.createClass({
 			<div className = "footer">
 				<div className = "footer-container">
 					<div className="white">©Designed and Built by Kenneth Song</div>
-					<div className="white footer-padding">Last Updated: 2016, August 16th</div>	
+					<div className="white footer-padding">Last Updated: 2016, August 19th</div>	
 				</div>
 			</div>
 		);
